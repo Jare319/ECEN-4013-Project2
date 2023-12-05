dde4ea54c844bdd113325b153b63ff4787693449
Jarrett <jarrettcrump@gmail.com>
2023-12-05 13:16:40 -0600

    Start screen almost done

--- GUI/src/StartScreen.java
+++ GUI/src/StartScreen.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
 
 import com.fazecast.jSerialComm.*;
 
@@ -11,34 +12,73 @@ public class StartScreen extends JPanel{
     
     private SerialPort[] allPorts;
     private SerialPort[] activePorts;
-    private int hgap = 5;
-    private int vgap = 5;
-    private JPanel comPanel, startPanel;
-    private JComboBox comSelector;
+    private String[] portNames = new String[10];
+    private int hgap = 10;
+    private int vgap = 10;
+    private JPanel comPanel, startPanel, comboBoxPanel, comButtonPanel, startButtonPanel, comButtonBottomPanel;
+    private JComboBox<String> comSelector;
     private JButton startButton, detectPortsButton;
 
     StartScreen() {
-        detectPorts();
+        this.setPreferredSize(new Dimension(300,200));
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.setBorder(new EmptyBorder(vgap,hgap,vgap,hgap));
+
         comPanel = new JPanel();
-        comPanel.setPreferredSize(new Dimension(300,300));
-        comPanel.setLayout(new BorderLayout(hgap,vgap));
-        comPanel.setBorder(new EmptyBorder(10,10,10,10));
-        JPanel test = new JPanel();
-        test.setPreferredSize(new Dimension(100,10));
+        comPanel.setPreferredSize(new Dimension(300,100));
+        comPanel.setLayout(new BoxLayout(comPanel, BoxLayout.Y_AXIS));
+        comPanel.setBorder(new TitledBorder("Select COM Port To Inspect"));
+
+        comboBoxPanel = new JPanel();
+        //comboBoxPanel.setPreferredSize(new Dimension(300,50));
+        comboBoxPanel.setSize(WIDTH, 75);
+        comboBoxPanel.setBorder(new EmptyBorder(5,5,5,5));
         String[] testPorts = {"Test1", "Test2", "Test3"};
-        comSelector = new JComboBox<>(testPorts);
-        comSelector.setSize(new Dimension(100,10));
+        comSelector = new JComboBox<String>(testPorts);
+        comSelector.setPreferredSize(new Dimension(200,30));
+        comboBoxPanel.add(comSelector);
+
+        comButtonPanel = new JPanel();
+        comButtonPanel.setLayout(new BorderLayout());
+        comButtonPanel.setBorder(new EmptyBorder(5,5,5,5));
+        comButtonPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        comButtonBottomPanel = new JPanel();
+        comButtonBottomPanel.setLayout(new BorderLayout());
         detectPortsButton = new JButton("Detect COM Ports");
-        detectPortsButton.setPreferredSize(new Dimension(30,10));
-        test.add(comSelector);
-        comPanel.add(test, BorderLayout.CENTER);
-        comPanel.add(detectPortsButton, BorderLayout.SOUTH);
+        detectPortsButton.setBackground(Color.lightGray);
+        detectPortsButton.setForeground(Color.black);
+        detectPortsButton.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createRaisedBevelBorder(),BorderFactory.createEmptyBorder(2,5,2,5)));
+        detectPortsButton.addActionListener(e -> detectPorts());
+        comButtonBottomPanel.add(detectPortsButton, BorderLayout.LINE_END);
+        comButtonPanel.add(comButtonBottomPanel, BorderLayout.PAGE_END);
+
+        startPanel = new JPanel();
+        startPanel.setPreferredSize(new Dimension(300, 50));
+        startPanel.setLayout(new BorderLayout());
+        startButtonPanel = new JPanel();
+        startButtonPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        startButtonPanel.setLayout(new BorderLayout());
+        startButton = new JButton("Start Data Collection");
+        startButton.setBackground(Color.lightGray);
+        startButton.setForeground(Color.black);
+        startButton.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createRaisedBevelBorder(),BorderFactory.createEmptyBorder(2,5,2,5)));
+        startButtonPanel.add(startButton, BorderLayout.LINE_END);
+        startPanel.add(startButtonPanel, BorderLayout.PAGE_END);
+
+        comPanel.add(comboBoxPanel);
+        comPanel.add(comButtonPanel);
+        
         this.add(comPanel);
+        this.add(startPanel);
+        detectPorts();
     }
 
     public void detectPorts() {
         allPorts = SerialPort.getCommPorts();
+        comSelector.removeAllItems();
+        for (int i = 0; i < allPorts.length; i++) {
+            comSelector.addItem(allPorts[i].getSystemPortName());
+        }
     }
 
     // public void detectPorts() {
